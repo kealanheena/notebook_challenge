a09129931be67945cece1a8ef215885d8237a524
kealanheena <kealanheena@gmail.com>
2020-01-06 16:54:03 +0000

    add return notes function

--- spec/note-list-tests.js
+++ spec/note-list-tests.js
@@ -1,4 +1,6 @@
 var notesList = new NotesList();
+notesList.addNote("string1");
+notesList.addNote("string2");
 
 function testNotesArray() {
     assert.isTrue(Array.isArray(notesList.notes));
@@ -6,14 +8,18 @@ function testNotesArray() {
 
 
 function addNote() {
-    notesList.addNote("string1");
-    notesList.addNote("string2");
     assert.isTrue(notesList.notes[0] === "string1");
     assert.isTrue(notesList.notes[1] === "string2");
 };
 
+function testReturnNotes() {
+    notes = notesList.returnNotes();
+    assert.isTrue(notes[0] === "string1" && notes[1] === "string2")
+}
+
 
 testNotesArray();
 addNote();
+testReturnNotes()
 
 console.log(`${passedTests} Passed Tests`)
--- src/note-list-model.js
+++ src/note-list-model.js
@@ -4,5 +4,8 @@ function NotesList() {
 
 NotesList.prototype.addNote = function (note) {
     this.notes.push(note);
-
 };
+
+NotesList.prototype.returnNotes = function() {
+    return this.notes;
+}
