e642aa04d4720615a57e35ca86548e30b2a4a660
kealanheena <kealanheena@gmail.com>
2020-01-06 15:43:03 +0000

    add returnText function

--- spec/note-tests.js
+++ spec/note-tests.js
@@ -1,8 +1,15 @@
-function testNoteText(){
-    var note = new Note("My favourite language is JavaScript");
-    assert.isTrue(note.text === "My favourite language is JavaScript")
+var note = new Note("My favourite language is JavaScript");
+
+function testNoteText() {
+  assert.isTrue(note.text === "My favourite language is JavaScript");
 };
 
+function testReturnText() {
+  assert.isTrue(note.returnText() === "My favourite language is JavaScript");
+}
+console.log("hello");
+
 testNoteText();
+testReturnText();
 
 console.log(`${passedTests} Passed Tests`)
